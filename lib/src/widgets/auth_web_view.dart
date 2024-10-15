cb1a9bc3ea86f1f16737deae002bf68a8ea9cc1d
hakanmertb <20060337@stu.omu.edu.tr>
2024-10-15 12:31:12 +0300

    Update auth_web_view.dart

--- lib/src/widgets/auth_web_view.dart
+++ lib/src/widgets/auth_web_view.dart
@@ -1,4 +1,5 @@
 import 'dart:developer';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import '../models/oauth_provider.dart';
@@ -45,7 +46,6 @@ class _OAuthWebViewState extends State<OAuthWebView> {
       String model = androidInfo.model;
       String buildId = androidInfo.id;
       String osVersion = androidInfo.version.release;
-      // WebView.platform.getVersionInfo() kaldırıldı
       String webViewVersion = 'unknown';
 
       _userAgent =
@@ -55,8 +55,8 @@ class _OAuthWebViewState extends State<OAuthWebView> {
           '${packageInfo.appName}/$appVersion';
     } else if (defaultTargetPlatform == TargetPlatform.iOS) {
       IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
-      String osVersion = iosInfo.systemVersion; // ?? operatörü kaldırıldı
-      String model = iosInfo.model; // ?? operatörü kaldırıldı
+      String osVersion = iosInfo.systemVersion;
+      String model = iosInfo.model;
 
       _userAgent =
           'Mozilla/5.0 (${model.replaceAll(',', ';')}; CPU iPhone OS ${osVersion.replaceAll('.', '_')} like Mac OS X) '
@@ -75,6 +75,7 @@ class _OAuthWebViewState extends State<OAuthWebView> {
       final authorizationUrl =
           await AuthService.getAuthorizationUrl(widget.provider);
       log("User Agent: $_userAgent");
+
       final controller = WebViewController()
         ..setUserAgent(_userAgent ?? '')
         ..clearCache()
@@ -82,12 +83,19 @@ class _OAuthWebViewState extends State<OAuthWebView> {
         ..setJavaScriptMode(JavaScriptMode.unrestricted)
         ..setNavigationDelegate(
           NavigationDelegate(
-            onPageStarted: (_) =>
-                mounted ? setState(() => _isLoading = true) : null,
-            onPageFinished: (_) =>
-                mounted ? setState(() => _isLoading = false) : null,
+            onPageStarted: (String url) {
+              log('Page load started: $url', name: 'OAuthWebView');
+              if (mounted) setState(() => _isLoading = true);
+            },
+            onPageFinished: (String url) {
+              log('Page load finished: $url', name: 'OAuthWebView');
+              if (mounted) setState(() => _isLoading = false);
+            },
             onNavigationRequest: (NavigationRequest request) {
+              log('Navigation request: ${request.url}', name: 'OAuthWebView');
               if (request.url.startsWith(widget.provider.redirectUrl)) {
+                log('Redirect URL detected, processing...',
+                    name: 'OAuthWebView');
                 AuthService.handleRedirect(request.url, widget.provider)
                     .then((result) {
                   if (mounted) Navigator.of(context).pop(result);
@@ -97,25 +105,74 @@ class _OAuthWebViewState extends State<OAuthWebView> {
               return NavigationDecision.navigate;
             },
             onWebResourceError: (WebResourceError error) {
-              mounted
-                  ? setState(
-                      () => _error = 'WebView hatası: ${error.description}')
-                  : null;
+              log('WebView error: ${error.description}',
+                  name: 'OAuthWebView', error: error);
+              if (mounted)
+                setState(() => _error = 'WebView error: ${error.description}');
             },
           ),
         )
+        ..addJavaScriptChannel(
+          'FlutterChannel',
+          onMessageReceived: _onJavaScriptMessage,
+        )
+        ..setOnConsoleMessage(_onConsoleMessage)
         ..loadRequest(Uri.parse(authorizationUrl))
         ..setBackgroundColor(
           widget.backgroundColor ?? Colors.white,
         );
 
-      await controller
-          .runJavaScript('document.cookie = "";'); // Çerezleri temizle
-      mounted ? setState(() => _controller = controller) : null;
+      // JavaScript kanallarının eklenmesinden önce JavaScript'in doğru şekilde yüklendiğinden emin ol
+      await controller.runJavaScript('''
+        window.onerror = function(message, source, lineno, colno, error) {
+          FlutterChannel.postMessage(JSON.stringify({
+            type: 'error',
+            message: message,
+            source: source,
+            lineno: lineno,
+            colno: colno,
+            error: error ? error.stack : null
+          }));
+        };
+
+        var originalFetch = window.fetch;
+        window.fetch = function() {
+          FlutterChannel.postMessage(JSON.stringify({
+            type: 'fetch',
+            url: arguments[0],
+            options: arguments[1]
+          }));
+          return originalFetch.apply(this, arguments);
+        };
+      ''');
+
+      if (mounted) setState(() => _controller = controller);
     } catch (e) {
+      log('Error initializing WebView: $e', name: 'OAuthWebView', error: e);
       debugPrint(e.toString());
       rethrow;
     }
+  }
+
+  void _onJavaScriptMessage(JavaScriptMessage message) {
+    final data = jsonDecode(message.message);
+    switch (data['type']) {
+      case 'pageSource':
+        log('Page source: ${data['content']}', name: 'OAuthWebView');
+        break;
+      case 'error':
+        log('JavaScript error: ${data['message']} at ${data['source']}:${data['lineno']}:${data['colno']}',
+            name: 'OAuthWebView');
+        break;
+      case 'fetch':
+        log('Fetch request: ${data['url']}', name: 'OAuthWebView');
+        break;
+    }
+  }
+
+  void _onConsoleMessage(JavaScriptConsoleMessage message) {
+    log('Console ${message.level.name}: ${message.message}',
+        name: 'OAuthWebView');
   }
 
   @override
