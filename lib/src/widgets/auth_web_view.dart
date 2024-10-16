1f9b194a2c23d526403b7aa6e259110a99c06b4f
hakanmertb <20060337@stu.omu.edu.tr>
2024-10-16 13:19:09 +0300

    last

--- lib/src/widgets/auth_web_view.dart
+++ lib/src/widgets/auth_web_view.dart
@@ -1,5 +1,4 @@
-import 'dart:developer';
-import 'dart:convert';
+import 'dart:developer' as developer;
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import '../models/oauth_provider.dart';
@@ -13,11 +12,12 @@ class OAuthWebView extends StatefulWidget {
   final Widget? loadingWidget;
   final Color? backgroundColor;
 
-  const OAuthWebView(
-      {super.key,
-      required this.provider,
-      this.loadingWidget,
-      this.backgroundColor});
+  const OAuthWebView({
+    super.key,
+    required this.provider,
+    this.loadingWidget,
+    this.backgroundColor,
+  });
 
   @override
   State<OAuthWebView> createState() => _OAuthWebViewState();
@@ -36,36 +36,31 @@ class _OAuthWebViewState extends State<OAuthWebView> {
   }
 
   Future<void> _getUserAgent() async {
-    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-    PackageInfo packageInfo = await PackageInfo.fromPlatform();
-    String appVersion = packageInfo.version;
+    try {
+      final deviceInfo = DeviceInfoPlugin();
+      final packageInfo = await PackageInfo.fromPlatform();
+      final appVersion = packageInfo.version;
 
-    if (defaultTargetPlatform == TargetPlatform.android) {
-      AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
-      String manufacturer = androidInfo.manufacturer;
-      String model = androidInfo.model;
-      String buildId = androidInfo.id;
-      String osVersion = androidInfo.version.release;
-      String webViewVersion = 'unknown';
-
+      if (defaultTargetPlatform == TargetPlatform.android) {
+        final androidInfo = await deviceInfo.androidInfo;
+        _userAgent =
+            'Mozilla/5.0 (Linux; Android ${androidInfo.version.release}; ${androidInfo.model}) '
+            'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/Unknown Mobile Safari/537.36 '
+            '${packageInfo.appName}/$appVersion';
+      } else if (defaultTargetPlatform == TargetPlatform.iOS) {
+        final iosInfo = await deviceInfo.iosInfo;
+        _userAgent =
+            'Mozilla/5.0 (${iosInfo.model}; CPU iPhone OS ${iosInfo.systemVersion.replaceAll('.', '_')} like Mac OS X) '
+            'AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1 '
+            '${packageInfo.appName}/$appVersion';
+      } else {
+        _userAgent =
+            'Mozilla/5.0 (Unknown; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36';
+      }
+    } catch (e) {
+      developer.log('Error getting user agent: $e', name: 'OAuthWebView');
       _userAgent =
-          'Mozilla/5.0 (Linux; Android $osVersion; $manufacturer $model Build/$buildId) '
-          'AppleWebKit/537.36 (KHTML, like Gecko) '
-          'Chrome/$webViewVersion Mobile Safari/537.36 '
-          '${packageInfo.appName}/$appVersion';
-    } else if (defaultTargetPlatform == TargetPlatform.iOS) {
-      IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
-      String osVersion = iosInfo.systemVersion;
-      String model = iosInfo.model;
-
-      _userAgent =
-          'Mozilla/5.0 (${model.replaceAll(',', ';')}; CPU iPhone OS ${osVersion.replaceAll('.', '_')} like Mac OS X) '
-          'AppleWebKit/605.1.15 (KHTML, like Gecko) '
-          'Version/15.0 Mobile/15E148 Safari/604.1 '
-          '${packageInfo.appName}/$appVersion';
-    } else {
-      _userAgent =
-          'Mozilla/5.0 (Unknown; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36';
+          'Mozilla/5.0 (Unknown) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Mobile Safari/537.36';
     }
   }
 
@@ -74,105 +69,54 @@ class _OAuthWebViewState extends State<OAuthWebView> {
       await _getUserAgent();
       final authorizationUrl =
           await AuthService.getAuthorizationUrl(widget.provider);
-      log("User Agent: $_userAgent");
 
       final controller = WebViewController()
         ..setUserAgent(_userAgent ?? '')
-        ..clearCache()
-        ..clearLocalStorage()
         ..setJavaScriptMode(JavaScriptMode.unrestricted)
-        ..setNavigationDelegate(
-          NavigationDelegate(
-            onPageStarted: (String url) {
-              log('Page load started: $url', name: 'OAuthWebView');
-              if (mounted) setState(() => _isLoading = true);
-            },
-            onPageFinished: (String url) {
-              log('Page load finished: $url', name: 'OAuthWebView');
-              if (mounted) setState(() => _isLoading = false);
-            },
-            onNavigationRequest: (NavigationRequest request) {
-              log('Navigation request: ${request.url}', name: 'OAuthWebView');
-              if (request.url.startsWith(widget.provider.redirectUrl)) {
-                log('Redirect URL detected, processing...',
-                    name: 'OAuthWebView');
-                AuthService.handleRedirect(request.url, widget.provider)
-                    .then((result) {
-                  if (mounted) Navigator.of(context).pop(result);
-                });
-                return NavigationDecision.prevent;
-              }
-              return NavigationDecision.navigate;
-            },
-            onWebResourceError: (WebResourceError error) {
-              log('WebView error: ${error.description}',
-                  name: 'OAuthWebView', error: error);
-              if (mounted)
-                setState(() => _error = 'WebView error: ${error.description}');
-            },
-          ),
-        )
-        ..addJavaScriptChannel(
-          'FlutterChannel',
-          onMessageReceived: _onJavaScriptMessage,
-        )
-        ..setOnConsoleMessage(_onConsoleMessage)
-        ..loadRequest(Uri.parse(authorizationUrl))
-        ..setBackgroundColor(
-          widget.backgroundColor ?? Colors.white,
-        );
+        ..setNavigationDelegate(_buildNavigationDelegate())
+        ..addJavaScriptChannel('FlutterChannel',
+            onMessageReceived: _onJavaScriptMessage)
+        ..setBackgroundColor(widget.backgroundColor ?? Colors.white);
 
-      // JavaScript kanallarının eklenmesinden önce JavaScript'in doğru şekilde yüklendiğinden emin ol
-      await controller.runJavaScript('''
-        window.onerror = function(message, source, lineno, colno, error) {
-          FlutterChannel.postMessage(JSON.stringify({
-            type: 'error',
-            message: message,
-            source: source,
-            lineno: lineno,
-            colno: colno,
-            error: error ? error.stack : null
-          }));
-        };
-
-        var originalFetch = window.fetch;
-        window.fetch = function() {
-          FlutterChannel.postMessage(JSON.stringify({
-            type: 'fetch',
-            url: arguments[0],
-            options: arguments[1]
-          }));
-          return originalFetch.apply(this, arguments);
-        };
-      ''');
+      await controller.loadRequest(Uri.parse(authorizationUrl));
 
       if (mounted) setState(() => _controller = controller);
     } catch (e) {
-      log('Error initializing WebView: $e', name: 'OAuthWebView', error: e);
-      debugPrint(e.toString());
-      rethrow;
+      developer.log('Error initializing WebView: $e', name: 'OAuthWebView');
+      setState(() => _error = 'Failed to initialize. Please try again.');
     }
+  }
+
+  NavigationDelegate _buildNavigationDelegate() {
+    return NavigationDelegate(
+      onPageStarted: (String url) {
+        if (mounted) setState(() => _isLoading = true);
+      },
+      onPageFinished: (String url) async {
+        if (mounted) setState(() => _isLoading = false);
+      },
+      onNavigationRequest: (NavigationRequest request) {
+        if (request.url.startsWith(widget.provider.redirectUrl)) {
+          AuthService.handleRedirect(request.url, widget.provider)
+              .then((result) {
+            if (mounted) Navigator.of(context).pop(result);
+          });
+          return NavigationDecision.prevent;
+        }
+        return NavigationDecision.navigate;
+      },
+      onWebResourceError: (WebResourceError error) {
+        developer.log('WebView error: ${error.description}',
+            name: 'OAuthWebView');
+        if (mounted) {
+          setState(() => _error = 'An error occurred. Please try again.');
+        }
+      },
+    );
   }
 
   void _onJavaScriptMessage(JavaScriptMessage message) {
-    final data = jsonDecode(message.message);
-    switch (data['type']) {
-      case 'pageSource':
-        log('Page source: ${data['content']}', name: 'OAuthWebView');
-        break;
-      case 'error':
-        log('JavaScript error: ${data['message']} at ${data['source']}:${data['lineno']}:${data['colno']}',
-            name: 'OAuthWebView');
-        break;
-      case 'fetch':
-        log('Fetch request: ${data['url']}', name: 'OAuthWebView');
-        break;
-    }
-  }
-
-  void _onConsoleMessage(JavaScriptConsoleMessage message) {
-    log('Console ${message.level.name}: ${message.message}',
-        name: 'OAuthWebView');
+    developer.log('Received message from JavaScript', name: 'OAuthWebView');
   }
 
   @override
